3a7a0541b7cd27ef2967057a0c0658595de67c81
muskanAsrani <44089077+muskanAsrani@users.noreply.github.com>
2021-07-29 14:33:28 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Bank
+{
+	public class Program
+	{
+
+		static void Main()
+		{
+			Class1 c = new Class1();
+			c.deposit(10,20);
+			c.withdraw(20, 20);
+
+		}
+	}
+}
